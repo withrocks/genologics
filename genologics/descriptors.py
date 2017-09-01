a9b6b7f3c870c92128ac5ac49d9cb8acfa114fe9
Steinar Sturlaugsson <costeinar@gmail.com>
2017-09-01 10:35:38 +0200

    Add length to UdfDictionary
    The UdfDictionary was mostly dictionary-like, but it was missing the
    __len__ method.

--- genologics/descriptors.py
+++ genologics/descriptors.py
@@ -228,6 +228,9 @@ class UdfDictionary(object):
     def __getitem__(self, key):
         return self._lookup[key]
 
+    def __len__(self):
+        return len(self._lookup)
+
     def __setitem__(self, key, value):
         self._lookup[key] = value
         for node in self._elems:
